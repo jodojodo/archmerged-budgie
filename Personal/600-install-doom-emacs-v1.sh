38f4bb19be05123dc89b1f62af0faf4263c13518
Jonas Siewert <jonasonweb@gmail.com>
2019-11-27 00:23:00 +0100

    Skript für Texlive und Doom erstellt

---
+++ Personal/600-install-doom-emacs-v1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+echo "Clone and install Doom Emacs"
+
+if [ -d $HOME"/.emacs.d" ]; then
+	echo "Create Backup of old .emacs.d folder?? (Y|n)"
+	read input
+	if [ "$input" != "n" ]; then
+		echo "Create Backup (and delete old Backup if necessary)"
+		[ -d $HOME"/.emacs.d.bak" ] && rm -rf $HOME"/.emacs.d.bak"
+		mv $HOME"/.emacs.d" $HOME"/.emacs.d.bak" 
+	else
+		echo "Remove old .emacs.d folder."
+		rm -rf $HOME"/.emacs.d"
+	fi
+fi
+git clone https://github.com/hlissner/doom-emacs $HOME"/.emacs.d"
+
+echo "Run the doom install script"
+$HOME"/.emacs.d/bin/doom" install
+
+echo "################################################################"
+echo "###                Doom Emacs was installed                 ####"
+echo "################################################################"
